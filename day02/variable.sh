de4ae9e4cca9e6124c6968d29a6b9869b268f354
uc2706 <uc2706@ubuntu.myguest.virtualbox.org>
2024-02-21 11:53:25 +0530

    day02 script added

---
+++ day02/variable.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this is variabl script
+
+<< comment 
+anything written in it will not be executed
+comment
+
+name="uday"
+
+echo "name is  $name and todays date is $(date)"
+
+echo "Enter the user name"
+
+read -p "enter user name"  username
+
+echo "you enter the name : $username"
+
+
